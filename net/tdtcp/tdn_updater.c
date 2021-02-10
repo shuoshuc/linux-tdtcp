484566373c1d93ad26ed245c49ba793483c18e04
Shawn Chen <sschenshawn@gmail.com>
2021-02-09 21:44:21 -0800

    Downgrade net/tdtcp/tdn_updater.c logs to pr_debug().

--- net/tdtcp/tdn_updater.c
+++ net/tdtcp/tdn_updater.c
@@ -19,13 +19,13 @@ void tdn_update_handler(struct work_struct *work)
 	struct tdn_work_data *data = container_of(work, struct tdn_work_data,
 						  tdn_work);
 
-	pr_info("[tdn_updater_wq] %s: sk=%p, tdn=%u. Acquiring lock.\n",
+	pr_debug("[tdn_updater_wq] %s: sk=%p, tdn=%u. Acquiring lock.\n",
 		__FUNCTION__, data->sk, data->tdn_id);
 	lock_sock(data->sk);
-	pr_info("[tdn_updater_wq] %s: Lock acquired.\n", __FUNCTION__);
+	pr_debug("[tdn_updater_wq] %s: Lock acquired.\n", __FUNCTION__);
 	WRITE_ONCE(tcp_sk(data->sk)->curr_tdn_id, data->tdn_id);
 	release_sock(data->sk);
-	pr_info("[tdn_updater_wq] %s: Lock released.\n", __FUNCTION__);
+	pr_debug("[tdn_updater_wq] %s: Lock released.\n", __FUNCTION__);
 
 	kfree(data);
 }
