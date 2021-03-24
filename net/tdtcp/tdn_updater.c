e75459e342c3e3cda81009623c09c145a14e88b1
Shawn Chen <sschenshawn@gmail.com>
2021-03-23 21:43:49 -0700

    [Bug fix] Use unbound ordered workqueue and reduce TDN updates.
    `create_workqueue()` is deprecated, its parallelism and ordering is hard
    to configure and enforce. We therefore use `alloc_ordered_queue()`
    instead, which implements an unbound single-threaded-like workqueue with
    maximum active work item being 1. Besides, it seems to have better
    performance than the old workqueue.
    
    In high-frequency TDN switching scenarios, ICMP TDN change packets might
    experience delay and arrive in batches. Or there could just be multiple
    ICMP TDN change packets with the same TDN ID. To avoid the socket locking
    overhead in updating TDN, a condition check is added outside of the
    critical section to suppress unnecessary changes, i.e., curr_tdn_id
    already matches the new TDN.

--- net/tdtcp/tdn_updater.c
+++ net/tdtcp/tdn_updater.c
@@ -7,6 +7,7 @@
 #include <linux/module.h>
 #include <linux/workqueue.h>
 #include <linux/slab.h>
+#include <linux/time.h>
 #include <net/tdtcp.h>
 
 MODULE_LICENSE("GPL");
@@ -16,17 +17,29 @@ EXPORT_SYMBOL(tdn_updater_wq);
 
 void tdn_update_handler(struct work_struct *work)
 {
+	ktime_t start, end;
 	struct tdn_work_data *data = container_of(work, struct tdn_work_data,
 						  tdn_work);
+	WARN_ON(!data);
+	WARN_ON(!data->sk);
+	/* No need to set same TDN again. This saves locking overhead. */
+	if (data->tdn_id == READ_ONCE(tcp_sk(data->sk)->curr_tdn_id))
+		goto free;
 
-	pr_debug("[tdn_updater_wq] %s: sk=%p, tdn=%u. Acquiring lock.\n",
-		__FUNCTION__, data->sk, data->tdn_id);
+	pr_debug("[%s] sk=%p, tdn=%u. Acquiring lock.\n",
+		 __FUNCTION__, data->sk, data->tdn_id);
+
+	start = ktime_get();
 	lock_sock(data->sk);
-	pr_debug("[tdn_updater_wq] %s: Lock acquired.\n", __FUNCTION__);
 	WRITE_ONCE(tcp_sk(data->sk)->curr_tdn_id, data->tdn_id);
 	release_sock(data->sk);
-	pr_debug("[tdn_updater_wq] %s: Lock released.\n", __FUNCTION__);
+        end = ktime_get();
 
+	pr_debug("[%s] sk=%p, tdn=%u. Lock released. Took %lld usec.\n",
+		 __FUNCTION__, data->sk, data->tdn_id,
+		 (long long)ktime_to_us(ktime_sub(end, start)));
+
+free:
 	kfree(data);
 }
 EXPORT_SYMBOL(tdn_update_handler);
@@ -34,7 +47,7 @@ EXPORT_SYMBOL(tdn_update_handler);
 static int tdn_updater_wq_init(void)
 {
 	pr_info("tdn_updater_wq: %s create wq.\n", __FUNCTION__);
-	tdn_updater_wq = create_workqueue("tdn_updater");
+	tdn_updater_wq = alloc_ordered_workqueue("tdn_updater", WQ_MEM_RECLAIM);
 	BUG_ON(!tdn_updater_wq);
 
 	return 0;
